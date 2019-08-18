87826f0b0430da612324942a9ab47f59dd336db9
Manuel Hesselbarth <jumper85@email.de>
2019-08-18 12:26:41 +0200

    Change config structure

--- js/config/webpack.common.js
+++ js/config/webpack.common.js
@@ -8,7 +8,7 @@ module.exports = {
     },
     output: {
         filename: '[name].js',
-        path: path.resolve(__dirname, 'js/build')
+        path: path.resolve(__dirname, '../build')
     },
     module: {
         rules: [
